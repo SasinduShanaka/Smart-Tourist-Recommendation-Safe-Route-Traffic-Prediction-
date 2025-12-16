1dfcb806703d21d00b8a935299cec4f4647c56a6
Sasindu Shanaka <sskalubowilage@gmail.com>
2025-12-16 15:31:27 +0530

    Add Sri Lankan tourist place coordinates utility
    Introduces a utility module with a comprehensive dictionary of Sri Lankan tourist places and their coordinates. Provides functions to retrieve coordinates by place name and to list all available place names.

---
+++ backend/location_utils.py
@@ -0,0 +1,126 @@
+# Comprehensive coordinates for Sri Lankan tourist places
+PLACE_COORDINATES = {
+    "Wadduwa": [6.6646, 79.9283], "Payagala": [6.6667, 79.9833], "Induruwa": [6.4833, 80.0167],
+    "Maggona": [6.6333, 79.9667], "Beruwala Lighthouse": [6.4833, 79.9833], "Kalpitiya Lagoon": [8.2333, 79.7667],
+    "Talawila Beach": [8.2833, 79.8167], "Kandakuliya Beach": [8.3167, 79.8], "Norochcholai Beach": [8.2, 79.8167],
+    "Anawilundawa Wetland": [7.9833, 79.8333], "Melsiripura": [7.4167, 80.4167], "Ridi Viharaya": [7.3, 80.5],
+    "Ibbankatuwa Megalithic Tombs": [7.8, 80.6333], "Nalanda Gedige": [7.6667, 80.7167], "Riverston Peak": [7.5833, 80.75],
+    "Sera Ella Falls": [7.5667, 80.7833], "Pitawala Pathana": [7.5333, 80.7667], "Thelgamu Oya": [7.5, 80.8],
+    "Illukkumbura": [7.4833, 80.7167], "Deanston": [7.4667, 80.6833], "Thalpe Beach": [6.0333, 80.1167],
+    "Jungle Beach Galle": [6.0333, 80.2167], "Rumassala Sanctuary": [6.0167, 80.2167], "Koggala Lake": [5.9833, 80.3167],
+    "Madol Duwa": [5.9667, 80.3333], "Weherahena Temple": [5.95, 80.55], "Mulkirigala Rock Temple": [6.1167, 80.7333],
+    "Kalametiya Bird Sanctuary": [6.0833, 80.9333], "Ussangoda National Park": [6.05, 81.05], "Godawaya Port Ruins": [6.0667, 81.0167],
+    "Seetha Eliya": [6.95, 80.7833], "Gregory Lake": [6.95, 80.7833], "Hakgala Botanical Garden": [6.9167, 80.8],
+    "Lover's Leap Falls": [6.9333, 80.7667], "Lover's Leap Falls": [6.9333, 80.7667], "Moon Plains": [6.8, 80.7833], "Bogoda Wooden Bridge": [6.8167, 81.05],
+    "Dunhinda Falls": [6.9833, 81.05], "Namunukula Mountain": [6.9333, 81.15], "Demodara Nine Arch Bridge": [6.8833, 81.0333],
+    "Lipton's Seat": [6.7667, 80.9833], "Passara": [7.0333, 81.2], "Mahiyanganaya Hot Springs": [7.3, 81.0],
+    "Rantembe Reservoir": [7.2333, 80.95], "Loggal Oya": [7.2, 80.9167], "Galaboda Falls": [7.1833, 80.8833],
+    "Hiriketiya Beach": [5.9667, 80.7], "Nilwella Beach": [5.9833, 80.65], "Dickwella": [5.9667, 80.7167],
+    "Kudawella Blow Hole": [5.9833, 80.7333], "Tangalle Lagoon": [6.0167, 80.8], "Rekawa Beach": [6.05, 80.85],
+    "Rekawa Turtle Conservation": [6.05, 80.85], "Kalametiya Lagoon": [6.0833, 80.9167], "Udawalawa Reservoir": [6.4667, 80.8833],
+    "Udawalawa Elephant Transit Home": [6.4667, 80.9], "Balangoda": [6.65, 80.6833], "Belihuloya River": [6.7167, 80.7833],
+    "Suriyakanda": [6.7333, 80.7667], "Pahanthudawa Waterfall": [6.7, 80.7], "Bopath Ella": [6.7167, 80.3833],
+    "Kuruwita Erathna Path": [6.7, 80.3667], "Sri Pada View Point": [6.8, 80.5], "Delhousie Town": [6.8167, 80.5],
+    "Wangedigala": [6.6667, 80.4833], "Hawagala": [6.6833, 80.5167], "Ratnapura Town": [6.6833, 80.4],
+    "Gem Museum Ratnapura": [6.6833, 80.4], "Udakanda View Point": [6.6333, 80.3667], "Kalawana Forest Area": [6.5333, 80.4],
+    "Wathurawa Waterfall": [6.5167, 80.4167], "Kitulgala White Water Rafting": [6.9833, 80.4167], "Makandawa Forest Reserve": [6.95, 80.4333],
+    "Belilena Cave": [7.0, 80.45], "Yatiyantota": [6.9667, 80.3167], "Deraniyagala": [6.9333, 80.3333],
+    "Avissawella": [6.9667, 80.2167], "Seethawaka Botanical Garden": [6.9833, 80.2333], "Hanwella": [6.9167, 80.0833],
+    "Labugama Reservoir": [6.8667, 80.15], "Kalatuwawa Reservoir": [6.8833, 80.1667], "Horagolla National Park": [7.1833, 80.1333],
+    "Mirigama": [7.2333, 80.1167], "Veyangoda": [7.1667, 80.05], "Divulapitiya": [7.3, 80.0333],
+    "Minuwangoda": [7.1667, 80.0167], "Katunayake Lagoon": [7.1667, 79.9], "Seeduwa": [7.1333, 79.9],
+    "Ja-Ela": [7.0833, 79.9], "Ekala": [7.0833, 79.9], "Wattala Mangroves": [7.0, 79.9],
+    "Kelaniya Raja Maha Vihara": [6.9667, 79.9167], "Biyagama": [6.95, 79.9833], "Hanwella Bridge Area": [6.9167, 80.0833],
+    "Kaduwela": [6.9333, 79.9833], "Malabe Wetlands": [6.9167, 80.0], "Jaffna Fort": [9.6667, 80.0167],
+    "Nallur Kandaswamy Temple": [9.6833, 80.0167], "Keerimalai Hot Springs": [9.8333, 80.1167], "Casuarina Beach": [9.8333, 80.0167],
+    "Point Pedro Lighthouse": [9.8167, 80.2333], "Nagadeepa Temple": [9.55, 79.7667], "Chundikulam National Park": [9.3667, 80.4333],
+    "Delft Island": [9.5, 79.7], "Kayts Island": [9.6167, 79.85], "Analaitivu Island": [9.6, 79.8833],
+    "Vavuniya Town": [8.75, 80.5], "Madukanda Vihara": [8.7167, 80.4833], "Iranamadu Tank": [9.3333, 80.4333],
+    "Mullaitivu Beach": [9.2667, 80.8167], "Nandikadal Lagoon": [9.45, 80.65], "Kilinochchi War Memorial": [9.3833, 80.4],
+    "Elephant Pass Lagoon": [9.3, 80.4167], "Kokkilai Lagoon": [8.9167, 80.95], "Nayaru Beach": [9.0333, 80.8667],
+    "Mullaitivu Lagoon": [9.2667, 80.8167], "Mankulam": [9.0, 80.6667], "Oddusuddan": [9.15, 80.5833],
+    "Puthukkudiyiruppu": [9.3, 80.5667], "Thiruketheeswaram Temple": [8.7667, 79.9667], "Mannar Fort": [8.9833, 79.9],
+    "Adam's Bridge Viewpoint": [9.1167, 79.5333], "Giant's Tank": [8.9333, 80.1], "Vankalai Sanctuary": [8.8333, 79.9],
+    "Pesalai Beach": [9.0833, 79.8667], "Arippu Fort": [8.8667, 79.85], "Trincomalee Harbour": [8.5833, 81.2333],
+    "Fort Frederick": [8.5833, 81.2333], "Nilaveli Lagoon": [8.7, 81.1833], "Kanniya Hot Springs": [8.6, 81.2],
+    "Marble Beach Trincomalee": [8.6667, 81.2167], "Seruwila Mangala Raja Maha Vihara": [8.4833, 81.2833], "Somawathiya National Park": [8.05, 81.2],
+    "Kuchchaveli Beach": [8.8333, 81.0333], "Uppuveli Beach": [8.65, 81.2167], "Sampur Beach": [8.6, 81.3],
+    "Padaviya Tank": [8.9333, 80.75], "Kebithigollewa": [8.7833, 80.7], "Horowpothana National Park": [8.5333, 80.6167],
+    "Kahatagasdigiliya": [8.3167, 80.6], "Nochchiyagama": [8.3, 80.3], "Madawachchiya": [8.55, 80.4833],
+    "Eppawala Phosphate Mine": [8.3833, 80.5167], "Ritigala Strict Nature Reserve": [8.05, 80.65], "Kala Wewa": [8.0333, 80.45],
+    "Avukana Buddha Statue": [8.05, 80.5], "Sigiriya Rock Fortress": [7.9571, 80.7603], "Dambulla Cave Temple": [7.8567, 80.6483],
+    "Polonnaruwa Ancient City": [7.9403, 81.0188], "Anuradhapura Ancient City": [8.3114, 80.4037], "Temple of the Tooth Kandy": [7.2936, 80.6408],
+    "Kandy Lake": [7.2906, 80.6431], "Royal Botanical Gardens Peradeniya": [7.2719, 80.5972], "Bahirawakanda Temple": [7.3061, 80.6339],
+    "Udawattakele Forest Reserve": [7.3014, 80.6492], "Hanthana Mountain Range": [7.2536, 80.6544], "Knuckles Mountain Range": [7.4333, 80.8],
+    "Pinnawala Elephant Orphanage": [7.2981, 80.3867], "Yala National Park": [6.3667, 81.5167], "Bundala National Park": [6.1917, 81.2167],
+    "Udawalawe National Park": [6.4833, 80.8833], "Wilpattu National Park": [8.5, 80.0], "Minneriya National Park": [8.0333, 80.9],
+    "Kaudulla National Park": [8.1167, 80.85], "Galle Fort": [6.0283, 80.2167], "Galle Lighthouse": [6.0253, 80.2169],
+    "Dutch Reformed Church Galle": [6.0281, 80.2172], "Unawatuna Beach": [6.0106, 80.2492], "Mirissa Beach": [5.9456, 80.4706],
+    "Mirissa Whale Watching": [5.9469, 80.4706], "Weligama Bay": [5.9731, 80.4289], "Bentota Beach": [6.4258, 80.0033],
+    "Bentota River Safari": [6.4236, 80.0019], "Hikkaduwa Beach": [6.1392, 80.1019], "Hikkaduwa Coral Sanctuary": [6.1319, 80.0972],
+    "Arugam Bay": [6.8406, 81.8361], "Pottuvil Point": [6.8681, 81.8358], "Kumana National Park": [6.5667, 81.6833],
+    "Ella Rock": [6.8697, 81.0478], "Little Adam's Peak": [6.8656, 81.0489], "Ravana Falls": [6.8597, 81.0536],
+    "Ravana Cave": [6.8572, 81.0519], "Ella Gap": [6.8667, 81.0467], "Nuwara Eliya Town": [6.9497, 80.7891],
+    "Victoria Park Nuwara Eliya": [6.9528, 80.7708], "Horton Plains National Park": [6.8089, 80.8019], "World's End Horton Plains": [6.8072, 80.8006],
+    "Baker's Falls": [6.7931, 80.8092], "Ambewela Farms": [6.9106, 80.8264], "Nanu Oya": [6.9333, 80.7667],
+    "Ramboda Falls": [7.0308, 80.6672], "Devon Falls": [6.9831, 80.6603], "St. Clair's Falls": [6.8889, 80.6106],
+    "Bambarakanda Falls": [6.8156, 80.8078], "Diyaluma Falls": [6.8183, 81.0242], "Adam's Peak": [6.8096, 80.4994],
+    "Sinharaja Forest Reserve": [6.4, 80.45], "Negombo Beach": [7.2128, 79.8358], "Negombo Lagoon": [7.2167, 79.8333],
+    "Muthurajawela Wetlands": [7.0333, 79.8833], "Colombo National Museum": [6.9108, 79.8617], "Gangaramaya Temple": [6.9167, 79.8556],
+    "Viharamahadevi Park": [6.9128, 79.8611], "Galle Face Green": [6.9294, 79.8469], "Independence Square": [6.9031, 79.8692],
+    "Beira Lake": [6.9317, 79.8539], "Mount Lavinia Beach": [6.8333, 79.8667], "Talangama Wetland": [6.9, 79.9667],
+    "Diyatha Uyana": [6.8931, 79.8881], "Dehiwala Zoo": [6.8569, 79.8747], "Pigeon Island National Park": [8.7028, 81.2403],
+    "Nilaveli Beach": [8.6931, 81.1892], "Koneswaram Temple": [8.5717, 81.2325], "Pasikuda Beach": [7.9333, 81.55],
+    "Kalkudah Beach": [7.9, 81.5333], "Batticaloa Lagoon": [7.7167, 81.7], "Batticaloa Lighthouse": [7.7206, 81.6989],
+    "Polonnaruwa Gal Vihara": [7.9681, 81.0008], "Parakrama Samudra": [7.9303, 80.9897], "Mihintale": [8.3508, 80.5069],
+    "Isurumuniya Temple": [8.3431, 80.3981], "Jaya Sri Maha Bodhi": [8.3464, 80.3967], "Ruwanwelisaya Stupa": [8.3486, 80.3964],
+    "Jetavanaramaya Stupa": [8.3519, 80.3975], "Abhayagiri Dagoba": [8.3642, 80.3956], "Twin Ponds Anuradhapura": [8.3506, 80.3936],
+    "Lankarama": [8.3639, 80.4006], "Thuparamaya": [8.3508, 80.3972], "Mihintale Rock": [8.3528, 80.5089],
+    "Sigiriya Museum": [7.9536, 80.7564], "Pidurangala Rock": [7.9606, 80.7597], "Matale Spice Garden": [7.4667, 80.6167],
+    "Aluvihare Rock Temple": [7.5644, 80.6119], "Sembuwatta Lake": [7.3833, 80.7833], "Manigala Rajamaha Viharaya": [7.3667, 80.7667],
+    "Ahas Ganga River": [6.8667, 80.8833], "Wewurukannala Vihara": [5.95, 80.5333], "Hummanaya Blow Hole": [5.9333, 80.7167],
+    "Dondra Head Lighthouse": [5.9233, 80.5878], "Polhena Beach": [5.9489, 80.5556], "Mawella Beach": [5.9708, 80.5172],
+    "Silent Beach Matara": [5.9472, 80.5494], "Paravi Duwa Temple": [5.9528, 80.5428], "Kataragama Temple": [6.4133, 81.3347],
+    "Sella Kataragama": [6.4167, 81.3333], "Magul Maha Viharaya": [6.4667, 81.5667], "Okanda Devalaya": [6.7833, 81.7833],
+    "Panama Beach": [6.7667, 81.8], "Kumana Bird Sanctuary": [6.5667, 81.6833], "Muhudu Maha Viharaya": [6.8, 81.8],
+    "Lahugala National Park": [7.0667, 81.7], "Gal Oya National Park": [7.2667, 81.4167], "Maduru Oya National Park": [7.7, 81.0],
+    "Dambana Indigenous Village": [7.3167, 81.0667], "Mahaweli River": [7.2833, 80.6333], "Maha Oya River": [7.15, 80.05],
+    "Kandalama Lake": [7.9167, 80.6667], "Kandalama Tank": [7.9167, 80.6833], "Popham's Arboretum": [7.8333, 80.6167],
+    "Aukana Buddha Statue": [8.3667, 80.5], "Yapahuwa Rock Fortress": [7.8333, 80.3667], "Dambadeniya Ancient City": [7.3, 80.15],
+    "Panduwasnuwara Ancient City": [7.6167, 80.0833], "Kurunegala Ethagala": [7.4833, 80.3667], "Kurunegala Lake": [7.4833, 80.3667],
+    "Ibbagala Raja Maha Viharaya": [7.5, 80.4], "Batalagoda Lake": [7.2167, 80.0167], "Weuda Arachchi Walauwa": [7.2333, 80.0333],
+    "Mundel Lake": [7.1333, 79.8333], "Dutch Fort Hammenhiel": [9.5833, 80.2167], "Naguleswaram Temple": [9.8167, 80.2167],
+    "Dambakolapatuna Raja Maha Viharaya": [9.7333, 80.0167], "Keerimalai Beach": [9.8333, 80.1167], "Charty Beach": [9.8, 80.05],
+    "Nilavarai Well": [9.7667, 80.1667], "Maviddapuram Temple": [9.6333, 80.0667], "Valvettithurai Beach": [9.8167, 80.1667],
+    "Kantharodai Buddhist Ruins": [9.7667, 80.1], "Thanthirimale Raja Maha Viharaya": [8.7333, 80.2167], "Ranagalla Rajamaha Viharaya": [7.3, 80.75],
+    "Wasgamuwa National Park": [7.7333, 80.9333], "Flood Plains National Park": [8.65, 81.0167], "Angammedilla National Park": [7.7667, 81.0167],
+    "Chundikulam Bird Sanctuary": [9.3667, 80.4333], "Giant's Tank Sanctuary": [8.9333, 80.1], "Wilpattu Kali Villu": [8.4833, 80.0333],
+    "Madhu Church": [8.8, 80.1167], "Tabbowa Lake": [8.05, 79.9], "Puttalam Lagoon": [8.0333, 79.8333],
+    "Anawilundawa Sanctuary": [7.9833, 79.8333], "Chilaw Beach": [7.5833, 79.7917], "Marawila Beach": [7.45, 79.8167],
+    "Munneswaram Temple": [7.6667, 79.8333], "Udappuwa Beach": [7.9333, 79.75], "St. Anne's Church Thalawila": [8.2833, 79.8167],
+    "Vankalai Bird Sanctuary": [8.8333, 79.9], "Wilpattu Border Village": [8.5, 79.95], "Kalpitiya Dutch Fort": [8.2333, 79.7667],
+    "Kalpitiya Beach": [8.2333, 79.75], "Alankuda Beach": [8.2833, 79.7667], "Bar Reef Marine Sanctuary": [8.5167, 79.8333],
+    "Talawila Church": [8.2833, 79.8167], "Udawalawa Wimalasurendra Reservoir": [6.4667, 80.8833], "Uda Walawe Reservoir Bungalow": [6.4667, 80.9],
+    "Elephant Transit Home Museum": [6.4667, 80.9]
+}
+
+def get_place_coordinates(place_name):
+    """
+    Get coordinates for a given place name.
+    
+    Args:
+        place_name (str): Name of the place
+        
+    Returns:
+        list: [latitude, longitude] or None if place not found
+    """
+    return PLACE_COORDINATES.get(place_name)
+
+def get_all_place_names():
+    """
+    Get list of all place names that have coordinates.
+    
+    Returns:
+        list: List of place names
+    """
+    return list(PLACE_COORDINATES.keys())
